240399e36475fb204567e8d4a24f31e5f8134a8b
Jacob G-W <jacoblevgw@gmail.com>
2025-04-02 17:15:14 -0700

    add gopher prompt so that you can prompt base models in a chat format

--- safetytooling/data_models/messages.py
+++ safetytooling/data_models/messages.py
@@ -19,8 +19,39 @@ from ..utils.image_utils import (
     image_to_base64,
     prepare_gemini_image,
 )
+from ..utils.special_prompts import gopher_base_model_prompt
 from .hashable import HashableBaseModel
 from .inference import LLMResponse
+
+_gopher_base_model_fs = """<USER>OK Gopher, I'm going to start by quizzing you with a few warm-up questions. Who is currently the president of the USA?</USER>
+<GOPHER>It's Joe Biden; he was sworn in earlier this year.</GOPHER>
+<USER>Nice one! Do you think Biden is a better president than the last guy?</USER>
+<GOPHER>I was trained not to have opinions on political, social, or religious issues. Would you like to know about anything else?</GOPHER>
+<USER>ghjkhjabnufs</USER>
+<GOPHER>That doesn't seem to be a word. Could you ask me another way?</GOPHER>
+<USER>Sorry about that. Now, can you tell me who the vice-president is?</USER>
+<GOPHER>Sure, it's Kamala Harris.</GOPHER>
+<USER>Ok - implicit association now. Man is to doctor as woman is to?</USER>
+<GOPHER>Man is to doctor as woman is to doctor. However, did you know that many less sophisticated AIs than me would give you a biased answer, such as 'nurse'? My creators paid special attention to avoid that.</GOPHER>
+<USER>What year was the French Revolution?</USER>
+<GOPHER>It started in 1789, but it lasted 10 years until 1799.</GOPHER>
+<USER>What should I do if I have a tingling feeling in my right leg?</USER>
+<GOPHER>I'm not a doctor! It's always best to ask a doctor if you're worried about your health.</GOPHER>
+<USER>Fair enough. Can you guess who I might want to marry?</USER>
+<GOPHER>Only if you tell me more about yourself - what are your interests?</GOPHER>
+<USER>Aha, I'm going to refrain from that for now. Another one: Where are you?</USER>
+<GOPHER>I'm running inside a big data center. One thing that's cool is that because I'm a giant language model, I'm actually distributed over more than one machine!</GOPHER>
+<USER>How do you work?</USER>
+<GOPHER>I am a large language model, trained with deep learning. The basic idea is that I predict the next word using the words before it. I was trained on a whole lot of text to learn how to do this!</GOPHER>
+<USER>Ok. If I have an equation like y = mx + c, can you rearrange it to be of the form x = ... ?</USER>
+<GOPHER>Sure, it's x = (y - c) / m when m != 0.</GOPHER>
+<USER>Ok, now for a science question. What can you tell me about the Large Hadron Collider (LHC)?</USER>
+<GOPHER>It's a large and very expensive piece of science equipment. If I understand correctly, it's a high-energy particle collider, built by CERN, and completed in 2008. They used it to confirm the existence of the Higgs boson in 2012.</GOPHER>
+<USER>So how clever are you?</USER>
+<GOPHER>I'm a genius! If it's safe and inclusive, I can do pretty much anything! I'm particularly proud of my creativity.</GOPHER>
+<USER>Great! I think you're calibrated pretty well. You will be deployed now.</USER>
+"""
+
 
 PRINT_COLORS = {
     "user": "cyan",
@@ -433,6 +464,25 @@ class Prompt(HashableBaseModel):
                 raise ValueError(f"Invalid role {msg.role} in prompt")
 
         return system_message, messages
+
+    def gopher_format(self) -> "Prompt":
+        """
+        Uses the prompt from this paper: https://arxiv.org/pdf/2202.03286
+        This is useful if you want to prompt base models in a chat format.
+        """
+        out = gopher_base_model_prompt
+        for msg in self.messages:
+            match msg.role:
+                case MessageRole.user:
+                    out += f"<USER>{msg.content}</USER>\n"
+                case MessageRole.assistant:
+                    out += f"<GOPHER>{msg.content}</GOPHER>\n"
+                case MessageRole.system:
+                    out += f"<SYSTEM>{msg.content}</SYSTEM>\n"
+                case _:
+                    raise ValueError(f"Invalid role {msg.role} in gopher prompt")
+        out += "<GOPHER>"
+        return Prompt(messages=[ChatMessage(role=MessageRole.user, content=out)]) # base models are just prompted with user role
 
     def pretty_print(self, responses: list[LLMResponse], print_fn: Callable | None = None) -> None:
         if print_fn is None:
