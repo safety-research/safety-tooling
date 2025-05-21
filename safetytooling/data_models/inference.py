572ac09369d89be1a670502524cbb224cadfb2b8
Phylliida Dev <phylliida.dev@gmail.com>
2025-05-20 18:02:25 -0700

    Made utils relative path
    This is important because otherwise import doesn't work right in certain setups

--- safetytooling/data_models/inference.py
+++ safetytooling/data_models/inference.py
@@ -5,7 +5,7 @@ from typing import Any, Literal, Union
 import openai
 import pydantic
 
-from safetytooling.data_models.utils import GeminiBlockReason, GeminiStopReason, SafetyRatings
+from .utils import GeminiBlockReason, GeminiStopReason, SafetyRatings
 
 from .hashable import HashableBaseModel
 
